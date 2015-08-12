67df9219bb364e423f52e318d884a546f8ac55a8
Rodrigo De Presbiteris <rpresb@walmart.com.br>
2015-08-12 18:00:08 -0300

    App running

--- app/www/js/main.js
+++ app/www/js/main.js
@@ -1,6 +1,8 @@
 var userData = {};
 var ua = navigator.userAgent;
 var logger = new Logger("main.js");
+var server = "http://10.24.2.145:3000";
+var lastId = 0;
 
 var device = {
 	Android: function() {
@@ -16,6 +18,39 @@ var device = {
 	}
 };
 
+var doAjax = function(url, type, data, callback) {
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		dataType: 'json',
+		timeout: 3000
+	})
+	.done(callback)
+	.complete(handleErrosOnComplete);
+};
+
+var handleErrosOnComplete = function(data, status) {
+	switch(data.status) {
+		case 0:
+			if(data.statusText === "timeout") {
+				logger.error("There are some problems to application server responding!");
+			} else {
+				logger.error("Application cannot respond.");
+			}
+		break;
+
+		case 500:
+		case 503:
+			logger.error("Internal Server Error");
+		break;
+
+		case 404:
+			logger.error("User not found!");
+		break;
+	}
+};
+
 Storage.prototype.setObj = function(key, obj) {
 	return this.setItem(key, JSON.stringify(obj));
 };
@@ -24,11 +59,37 @@ Storage.prototype.getObj = function(key) {
 	return JSON.parse(this.getItem(key));
 };
 
+var showMessage = function(data) {
+	if (lastId != data.id) {
+		lastId = data.id;
+
+		data.events.forEach(function (e) {
+			switch (e.type) {
+				case "color":
+					$("body").css("background-color", e.value);
+					break;
+				case "vibrate":
+					e.value.forEach(function(v) {
+						setTimeout(function() {
+							navigator.vibrate(v.time)
+						}, v.delay);
+					});
+					break;
+			}
+		});
+
+		$(".main-screen").html(JSON.stringify(data));
+	}
+}
+
 $(document).ready(function () {
 
-	navigator.vibrate(2000);
+	var timer = window.setInterval(function() {
+
+		doAjax(server + "/api/messages/receive", "GET", null, showMessage);
+
+	}, 1000);
 
 	logger.info("Hello");
-
 	logger.turnOffDebug();
 });
