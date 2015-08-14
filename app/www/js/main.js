d5c59854c073852301533cc1f7ff5f5c65781b58
Stacy DuBois <stacy.dubois@gmail.com>
2015-08-14 10:19:22 -0400

    Changing code to use wav files instead of m4a.

--- app/www/js/main.js
+++ app/www/js/main.js
@@ -162,7 +162,7 @@ var bookCollection = {
 		color: "",
 		image: "porquinho.svg",
 		vibrate: "three",
-		sound: ["happystarting.m4a","oink1.m4a"]
+		sound: ["happystarting.wav","oink1.wav"]
 	},
 	{
 		id: 1,
@@ -170,7 +170,7 @@ var bookCollection = {
 		text: "Um dia, eles resolveram deixar a casa de sua mãe e foram construir suas próprias casas na floresta.",
 		image: "casa.svg",
 		vibrate: "hammer",
-		sound: ["hammer3.m4a"]
+		sound: ["hammer3.wav"]
 	},
 	{
 		id: 2,
@@ -178,7 +178,7 @@ var bookCollection = {
 		text: "O porquinho Prático disse que faria sua casa de tijolos.",
 		image: "tijolo.svg",
 		vibrate: "hammer",
-		sound: ["hammer2.m4a"]
+		sound: ["hammer2.wav"]
 	},
 	{
 		id: 3,
@@ -186,7 +186,7 @@ var bookCollection = {
 		text: "O porquinho Heitor decidiu construir sua casa de madeira.",
 		image: "madeira.svg",
 		vibrate: "hammer",
-		sound: ["hammer3.m4a"]
+		sound: ["hammer3.wav"]
 	},
 	{
 		id: 4,
@@ -194,7 +194,7 @@ var bookCollection = {
 		text: "E o porquinho Cícero decidiu construir sua casa de palha.",
 		image: "palha.svg",
 		vibrate: "hammer",
-		sound: ["hammer1.m4a"]
+		sound: ["hammer1.wav"]
 	},
 	{
 		id: 5,
@@ -202,7 +202,7 @@ var bookCollection = {
 		text: "Uma noite, veio um lobo, bateu na casa de palha e queria entrar.",
 		image: "lobo.svg",
 		vibrate: "continuous",
-		sound: ["wolf1.m4a","knock1.m4a"]
+		sound: ["wolf1.wav","knock1.wav"]
 	},
 	{
 		id: 6,
@@ -210,7 +210,7 @@ var bookCollection = {
 		text: "O porquinho apavorado não abriu a porta. Então o lobo estufou o peito, soprou forte e a casa de palha voou pelos ares.",
 		image: "vento.svg",
 		vibrate: "continuous",
-		sound: ["blowing1.m4a"]
+		sound: ["blowing1.wav"]
 	},
 	{
 		id: 7,
@@ -218,7 +218,7 @@ var bookCollection = {
 		text: "Entao, o porquinho correu para a casa de madeira. O lobo chegou e bateu mas ninguém abriu a porta.",
 		image: "lobo.svg",
 		vibrate: "continuous",
-		sound: ["wolf2.m4a","knock2.m4a"]
+		sound: ["wolf2.wav","knock2.wav"]
 	},
 	{
 		id: 8,
@@ -226,7 +226,7 @@ var bookCollection = {
 		text: "Então o lobo estufou o peito, soprou forte e a casa de madeira voou pelos ares.",
 		image: "vento.svg",
 		vibrate: "continuous",
-		sound: ["blowing2.m4a"]
+		sound: ["blowing2.wav"]
 	},
 	{
 		id: 9,
@@ -234,7 +234,7 @@ var bookCollection = {
 		text: "Os porquinhos correram para a casa de tijolos.",
 		image: "porquinho.svg",
 		vibrate: "three",
-		sound: ["oink3.m4a"]
+		sound: ["oink3.wav"]
 	},
 	{
 		id: 10,
@@ -242,7 +242,7 @@ var bookCollection = {
 		text: "Como o porquinho Prático era esperto, deixou um caldeirão perto da porta. O lobo correu e caiu dentro do caldeirão com água fervendo e fugiu da casa. E assim, os três porquinhos viveram felizes na casa de tijolos.",
 		image: "casa.svg",
 		vibrate: "three",
-		sound: ["happyending.m4a"]
+		sound: ["happyending.wav"]
 	}
 
 	]};
